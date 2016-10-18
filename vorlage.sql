b9f3484fd700c749f8a6430591afff13b341ac07
Luther Blissett <luther.blissett@posteo.net>
2016-10-18 16:35:39 +0200

    vorlage.sql changed to .sql dump

--- vorlage.sql
+++ vorlage.sql
@@ -1,28 +1,68 @@
-standart(){
-drop database vorlage;
-create database vorlage;
-use vorlage;
-create table User(
-ID integer primary key,
-Name varchar(30),
-Password varchar(50),
-Email varchar(70)
-);
-create table Sensor(
-ID integer primary key,
-Sensor_Name varchar(20),
-User_ID integer,
-foreign key (User_ID) references User(ID)
-);
-}
+-- MySQL dump 10.13  Distrib 5.5.52, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: vorlage
+-- ------------------------------------------------------
+-- Server version	5.5.52-0ubuntu0.14.04.1
 
-wennNeuerSensorErkanntWurde(){
-use sensor;
-create table #sensorID(
-Sensor_ID varchar(20),
-temp float,
-humidity float
-foreign key (Sensor_ID) references Sensor(ID)
-);
-}
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Current Database: `vorlage`
+--
+
+CREATE DATABASE /*!32312 IF NOT EXISTS*/ `vorlage` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `vorlage`;
+
+--
+-- Table structure for table `Sensor`
+--
+
+DROP TABLE IF EXISTS `Sensor`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `Sensor` (
+  `ID` int(11) NOT NULL,
+  `Sensor_Name` varchar(20) DEFAULT NULL,
+  `User_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`ID`),
+  KEY `User_ID` (`User_ID`),
+  CONSTRAINT `Sensor_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `User` (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `User`
+--
+
+DROP TABLE IF EXISTS `User`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `User` (
+  `ID` int(11) NOT NULL,
+  `Name` varchar(30) DEFAULT NULL,
+  `Password` varchar(50) DEFAULT NULL,
+  `Email` varchar(70) DEFAULT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-10-15 16:14:35
