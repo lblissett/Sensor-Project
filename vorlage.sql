964bf6871c35954d9facc9277178cb548001a290
Leonard Franke <leonard.franke@ufz.de>
2016-10-20 18:20:13 +0200

    changed sql Data

--- vorlage.sql
+++ vorlage.sql
@@ -2,27 +2,44 @@ standart(){
 drop database vorlage;
 create database vorlage;
 use vorlage;
-create table User(
-ID integer primary key,
-Name varchar(30),
-Password varchar(50),
-Email varchar(70)
-);
-create table Sensor(
-ID integer primary key,
-Sensor_Name varchar(20),
-User_ID integer,
-foreign key (User_ID) references User(ID)
-);
-}
+CREATE TABLE `sensor` (
+  `pkid` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `userID` varchar(40) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `location` varchar(100) NOT NULL
+)
 
-wennNeuerSensorErkanntWurde(){
-use sensor;
-create table #sensorID(
-Sensor_ID varchar(20),
-temp float,
-humidity float
-foreign key (Sensor_ID) references Sensor(ID)
-);
+CREATE TABLE `sensor_datas` (
+  `pkid` int(11) NOT NULL,
+  `id_sensor` int(11) NOT NULL,
+  `time` datetime NOT NULL,
+  `temperature` double NOT NULL,
+  `humidity` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `user` (
+  `pkid` int(11) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+
+ALTER TABLE `sensor`
+  ADD PRIMARY KEY (`pkid`);
+ALTER TABLE `sensor_datas`
+  ADD PRIMARY KEY (`pkid`);
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`pkid`),
+  ADD UNIQUE KEY `pkid` (`pkid`);
+ALTER TABLE `sensor`
+  MODIFY `pkid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `sensor_datas`
+  MODIFY `pkid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user`
+  MODIFY `pkid` int(11) NOT NULL AUTO_INCREMENT;
+
+
 }
 
