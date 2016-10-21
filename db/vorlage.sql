e3557e208de9892830f0e94699ff1f02127907e7
Leonard Franke <leonard.franke@ufz.de>
2016-10-21 08:54:48 +0200

    updated other sql file

--- db/vorlage.sql
+++ db/vorlage.sql
@@ -23,46 +23,40 @@ CREATE DATABASE /*!32312 IF NOT EXISTS*/ `vorlage` /*!40100 DEFAULT CHARACTER SE
 
 USE `vorlage`;
 
---
--- Table structure for table `Sensor`
---
-
-DROP TABLE IF EXISTS `Sensor`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `Sensor` (
-  `ID` int(11) NOT NULL,
-  `Sensor_Name` varchar(20) DEFAULT NULL,
-  `User_ID` int(11) DEFAULT NULL,
-  PRIMARY KEY (`ID`),
-  KEY `User_ID` (`User_ID`),
-  CONSTRAINT `Sensor_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `User` (`ID`)
+CREATE TABLE `sensor` (
+  `pkid` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `userID` varchar(40) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `location` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Table structure for table `User`
---
-
-DROP TABLE IF EXISTS `User`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `User` (
-  `ID` int(11) NOT NULL,
-  `Name` varchar(30) DEFAULT NULL,
-  `Password` varchar(50) DEFAULT NULL,
-  `Email` varchar(70) DEFAULT NULL,
-  PRIMARY KEY (`ID`)
+CREATE TABLE `sensor_datas` (
+  `pkid` int(11) NOT NULL,
+  `id_sensor` int(11) NOT NULL,
+  `time` datetime NOT NULL,
+  `temperature` double NOT NULL,
+  `humidity` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+CREATE TABLE `user` (
+  `pkid` int(11) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `username` varchar(20) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `created` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Dump completed on 2016-10-15 16:14:35
+ALTER TABLE `sensor`
+  ADD PRIMARY KEY (`pkid`);
+ALTER TABLE `sensor_datas`
+  ADD PRIMARY KEY (`pkid`);
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`pkid`),
+  ADD UNIQUE KEY `pkid` (`pkid`);
+ALTER TABLE `sensor`
+  MODIFY `pkid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `sensor_datas`
+  MODIFY `pkid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user`
+  MODIFY `pkid` int(11) NOT NULL AUTO_INCREMENT;
