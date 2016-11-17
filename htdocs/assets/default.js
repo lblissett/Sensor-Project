0241fad8c7d9ae2cd3be1e1553c63abf8b09a6b1
Leonard Franke <leonard.franke@ufz.de>
2016-11-17 15:53:22 +0100

    bla

--- htdocs/assets/default.js
+++ htdocs/assets/default.js
@@ -360,7 +360,7 @@ $(document).ready(function () {
                                 // ---- Modal neu initialisieren ----
                                 $modal.modal();
                                 $('#labelInfoModal').text('Informationen zum Sensor "' + row.name + '"');
-                                setDiagram();
+                                setDiagram(response);
                             });
 
                         }).on('click', '.infos', function () {
@@ -380,106 +380,10 @@ $(document).ready(function () {
 
 
     <!-- D3 Minimalbeispiel Test-->
-    function setDiagram() {
-        var svg = d3.select("svg"),
-            margin = {top: 20, right: 80, bottom: 30, left: 50},
-            width = svg.attr("width") - margin.left - margin.right,
-            height = svg.attr("height") - margin.top - margin.bottom,
-            g = svg.append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    function setDiagram(response) {
 
-        var parseTime = d3.timeParse("%Y-%m-%d %H:%M:%S");
 
-        var x = d3.scaleTime().range([0, width]),
-            y = d3.scaleLinear().range([height, 0]),
-            z = d3.scaleOrdinal(d3.schemeCategory10);
 
-        var line = d3.line()
-            .curve(d3.curveBasis)
-            .x(function(d) {
-                return x(d.time);
-            })
-            .y(function(d) {
-                return y(d.sensordata);
-            });
-
-        d3.csv("/views/index/includes/sensor_datas.csv", type, function(error, data) {
-            if (error) throw error;
-
-            var values = data.columns.slice(3).map(function(id) {
-                return {
-                    id: id,
-                    values: data.map(function(d) {
-                        return {time: d.time, sensordata: d[id]};
-                    })
-                };
-            });
-
-            x.domain(d3.extent(data, function(d) {
-                return d.time;
-            }));
-
-            y.domain([
-                d3.min(values, function(c) {
-                    return d3.min(c.values, function(d) {
-                        return d.sensordata; });
-                }),
-                d3.max(values, function(c) {
-                    return d3.max(c.values, function(d) {
-                        return d.sensordata; }); })
-            ]);
-
-            z.domain(values.map(function(c) {
-                return c.id;
-            }));
-
-            g.append("g")
-                .attr("class", "axis axis--x")
-                .attr("transform", "translate(0," + height + ")")
-                .call(d3.axisBottom(x));
-
-            g.append("g")
-                .attr("class", "axis axis--y")
-                .call(d3.axisLeft(y))
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", "0.71em")
-                .attr("fill", "red")
-
-            var value = g.selectAll(".value")
-                .data(values)
-                .enter().append("g")
-                .attr("class", "value");
-
-            value.append("path")
-                .attr("class", "line")
-                .attr("d", function(d) {
-                    return line(d.values);
-                })
-                .style("stroke", function(d) {
-                    return z(d.id);
-                });
-
-            value.append("text")
-                .datum(function(d) {
-                    return {id: d.id, value: d.values[d.values.length - 1]};
-                })
-                .attr("transform", function(d) {
-                    return "translate(" + x(d.value.time) + "," + y(d.value.sensordata) + ")";
-                })
-                .attr("x", 3)
-                .attr("dy", "0.35em")
-                .style("font", "10px sans-serif")
-                .text(function(d) {
-                    return d.id;
-                });
-        });
-
-        function type(d, _, columns) {
-            d.time = parseTime(d.time);
-            for (var i = 1, n = columns.length, c; i < n; ++i) d[c = columns[i]] = +d[c];
-            return d;
-        }
     }
 
 
