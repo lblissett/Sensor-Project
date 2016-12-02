20afbf2fefe73a6e1a7be98011d7856acac6a642
Leonard Franke <leonard.franke@ufz.de>
2016-12-02 11:56:15 +0100

    fixed error with nodatas

--- htdocs/Model/ModelBase.php
+++ htdocs/Model/ModelBase.php
@@ -11,7 +11,7 @@ abstract class ModelBase
     public function getPdo()
     {
         if (self::$pdo === null) {
-            self::$pdo = new \PDO('mysql:host=localhost;dbname=mysql', 'root', 'mypass');
+            self::$pdo = new \PDO('mysql:host=mysql;dbname=vorlage', 'root', 'mypass');
         }
         return self::$pdo;
     }
